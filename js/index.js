b301a1f9ae520c7194bfab4c425633ea02de6ae2
Tim Gallant <tgallant@attn.com>
2017-06-03 01:49:36 -0700

    adds tag handling

--- js/index.js
+++ js/index.js
@@ -1,10 +1,64 @@
+/* global history,location */
+
 'use strict'
 
 import Layzr from 'layzr.js'
 
 const instance = Layzr()
 
-instance
-  .update()
-  .check()
-  .handlers(true)
+window.onload = () => {
+  initLayzr(instance)
+  checkHash()
+  handleTags()
+}
+
+function initLayzr (lzr) {
+  lzr
+    .update()
+    .check()
+    .handlers(true)
+}
+
+function checkHash () {
+  const hash = location.hash
+
+  if (hash !== '') {
+    const tagName = hash.substr(1)
+    const tag = document.querySelector(`[data-tag="${tagName}"]`)
+
+    if (tag) {
+      tag.classList.toggle('selected')
+    }
+  }
+}
+
+function handleTags () {
+  const tags = document.querySelectorAll('.tag')
+
+  Array.from(tags).forEach(tag => {
+    tag.addEventListener('click', toggleTag, false)
+  })
+}
+
+function toggleTag (e) {
+  const target = e.target
+
+  if (target.classList.contains('selected')) {
+    target.classList.toggle('selected')
+    history.pushState('', document.title, location.pathname)
+    return
+  }
+
+  const selected = document.querySelector('.selected')
+
+  if (selected) {
+    selected.classList.toggle('selected')
+  }
+
+  target.classList.toggle('selected')
+
+  const tag = target.innerText
+  const hash = encodeURIComponent(tag.toLowerCase())
+
+  history.pushState(null, null, `#${hash}`)
+}
