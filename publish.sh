874ad1b488d04a0a1a5eb8087e589c1ac6ac5379
Tim Gallant <tgallant@attn.com>
2017-06-04 17:28:02 -0700

    adds cloudflare cache purge

--- publish.sh
+++ publish.sh
@@ -4,3 +4,10 @@
 LOCATION=pics.timgallant.us
 DIR=build/
 aws s3 sync $DIR s3://$LOCATION --region us-west-2
+
+# Clear Cloudflare Cache
+curl -X DELETE "https://api.cloudflare.com/client/v4/zones/${CF_ZONE}/purge_cache" \
+     -H "X-Auth-Email: ${CF_EMAIL}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"purge_everything":true}'
